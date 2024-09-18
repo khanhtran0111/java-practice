f4deea63f21e61c9065406c41e61ce2e08fd08bf
Tran Gia Khanh <90601002+khanhtran0111@users.noreply.github.com>
2024-09-18 07:10:59 +0700

    changes

---
+++ Sol.java
@@ -0,0 +1,11 @@
+public class Sol {
+
+    static String printHello(){
+        System.out.print("HelloWorld");
+    }
+    public static void main(String[] args) {
+        Sol ans = new Sol();
+        System.out.println(ans.printHello());
+    }
+}
+
